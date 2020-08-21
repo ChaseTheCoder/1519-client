d3350a1a2ea0d9451c4d5391b3df939407e7fc55
ChaseTheCoder <chasefsheaff@gmail.com>
2020-08-21 15:38:39 -0500

    Nav component started

---
+++ src/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+export default function Nav() {
+    return (
+        <nav>
+            <a>1519</a>
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Ballot</a></li>
+                <li><a href="#">Feed</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+    );
+}
