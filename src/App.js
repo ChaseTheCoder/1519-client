db777365daf1f5e5b0b553bf71901002729fde67
ChaseTheCoder <chasefsheaff@gmail.com>
2020-08-21 23:50:17 -0500

    Modified Header and Nav

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Nav from './Nav/Nav';
 import About from './Pages/About';
 import Ballot from './Pages/Ballot';
 import Feed from './Pages/Feed';
-import Login from './Pages/Login'
+import Login from './Pages/Login';
+import Profile from './Pages/Profile';
 
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
-      {/* <Home /> */}
+      <Home />
       {/* <About /> */}
       {/* <Ballot /> */}
       {/* <Feed /> */}
-      <Login />
+      {/* <Login /> */}
+      {/* <Profile /> */}
     </div>
   );
 }
