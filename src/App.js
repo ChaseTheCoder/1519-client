4a20277e25e9e59093b3bf7690ac9f734cb4afe9
ChaseTheCoder <chasefsheaff@gmail.com>
2020-08-23 14:12:33 -0500

    servers working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,6 @@ import React from 'react';
 import routes from './config/routes';
 import Nav from './Nav/Nav';
 import './App.css';
-// import Home from './Pages/Home';
-// import About from './Pages/About';
-// import Ballot from './Pages/Ballot';
-// import Feed from './Pages/Feed';
-// import Login from './Pages/Login';
-// import Profile from './Pages/Profile';
-
 
 class App extends React.Component {
   render () {
