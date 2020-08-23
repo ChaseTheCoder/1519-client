6d6fa631e7e0a41cdfe2c62323bac81bc5944af9
ChaseTheCoder <chasefsheaff@gmail.com>
2020-08-23 14:14:01 -0500

    ballot change

--- src/BallotPanel/BallotPanel.js
+++ src/BallotPanel/BallotPanel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../Panel/Panel.css';
 
-export default function Login() {
+export default function Ballot() {
     return(
         <div className="panel">
             <h1>Login</h1>
