7efd2d83be576032d760ecfcfb3b19a6e1904f64
ChaseTheCoder <chasefsheaff@gmail.com>
2020-08-23 13:19:46 -0500

    began router and installed react router

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 export default function Nav() {
     return (
         <nav>
-            
             <ul>
-                <li><a id="logo"><span>15</span>19</a></li>
+                <li><NavLink id="logo"><span>15</span>19</NavLink></li>
                 <div className="route">
-                    <li><a id="route" href="#">About</a></li>
-                    <li><a id="route" href="#">Ballot</a></li>
-                    <li><a id="route" href="#">Feed</a></li>
-                    <li><a id="route" href="#">Login</a></li>
+                    <li><NavLink id="route" to="/about">About</NavLink></li>
+                    <li><NavLink id="route" to="/ballot">Ballot</NavLink></li>
+                    <li><NavLink id="route" to="/feed">Feed</NavLink></li>
+                    <li><NavLink id="route" to="/login">Login</NavLink></li>
                 </div>
-
             </ul>
         </nav>
     );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
 
-export default function Home() {
+const Home = () => {
     return (
         <div>
             <Header />
         </div>
-    )
+    );
 }
+
+export default Home;
